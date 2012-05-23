b47ba23218dc48dea6ab6e6f047243b9c65ec1b2
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-05-23 16:54:13 -0700

    mass and atomic querying done and tested

--- periodic/table.py
+++ periodic/table.py
@@ -1,9 +1,14 @@
+"""Docstring goes here."""
+
 import csv
 import os
 
 from sqlalchemy import Column, Integer, String, create_engine, Float
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+__author__ = 'Luis Naranjo'
+__email__ = 'luisnaranjo733@hotmail.com'
 
 # Database
 #========================================================================
@@ -16,8 +21,7 @@ Base = declarative_base()
 
 
 #========================================================================
-__author__ = 'Luis Naranjo'
-__email__ = 'luisnaranjo733@hotmail.com'
+
 
 
 class Element(Base):
@@ -31,8 +35,42 @@ class Element(Base):
     def __repr__(self):
         return "<Element('%s')>" % self.symbol
 
-
 #Base.metadata.create_all(engine)  # init table
+query = session.query(Element)
+
+def _type(_type):
+    """Takes a _type, and returns a string representation of its' real _type."""
+
+    try:
+        _type = float(_type)
+        if _type.is_integer():
+            return int
+        if not _type.is_integer():
+            return float
+    except ValueError:
+        return str
+
+
+def find(_input):
+    value = _type(_input)
+
+    if value is int:
+        return query.filter_by(atomic=_input).first()
+
+    if value is float:
+        return query.filter_by(mass=_input).first()
+
+# Testing _type func
+#========================================================================
+
+assert _type(1) == int
+assert _type(15.999) == float
+assert _type('hydrogen') == str
 
 
 
+# Testing
+#========================================================================
+hydrogen = query.filter_by(name='Hydrogen').first()
+assert hydrogen == find(1)
+assert hydrogen == find(1.00794)
