2555b3590612f5397cc5b9fba2a9bd940cc8aa83
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-05-24 16:44:49 -0700

    Added more scenarios for tests

--- tests/periodic_tests.py
+++ tests/periodic_tests.py
@@ -1,21 +1,23 @@
-import periodic as table
+import periodic
 
 # Testing type_ function
 #========================================================================
 
-def testtype_():
-    assert table.type_(1) == int
-    assert table.type_(15.999) == float
-    assert table.type_('hydrogen') == str
-    assert table.type_('H') == str
+def test_type_():
+    assert periodic.type_(1) == int
+    assert periodic.type_(15.999) == float
+    assert periodic.type_('hydrogen') == str
+    assert periodic.type_('H') == str
 
 # Testing database queries
 #========================================================================
 def test_element():
-    hydrogen = table.session.query(table.Element).filter_by(name='Hydrogen').first()
+    hydrogen = periodic.session.query(periodic.Element).filter_by(name='Hydrogen').first()
     tests = [
         1,
+        '1',
         1.00794,
+        '1.00794',
         'H',
         'h',
         'Hydrogen',
@@ -23,10 +25,10 @@ def test_element():
     ]
 
     for test in tests:
-        assert hydrogen == table.element(test)
+        assert hydrogen == periodic.element(test)
 
 def test_all():
-    testtype_()
+    test_type_()
     test_element()
 
 if __name__ == '__main__':
