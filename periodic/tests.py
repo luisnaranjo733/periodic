b8a4ca9a2ca70c28892e2683acd5b80cdc1802b7
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-05-23 21:28:47 -0700

    Moved tests to another file

---
+++ periodic/tests.py
@@ -0,0 +1,24 @@
+import table
+
+# Testing _type function
+#========================================================================
+
+assert table._type(1) == int
+assert table._type(15.999) == float
+assert table._type('hydrogen') == str
+assert table._type('H') == str
+
+# Testing database queries
+#========================================================================
+hydrogen = table._query.filter_by(name='Hydrogen').first()
+tests = [
+    1,
+    1.00794,
+    'H',
+    'h',
+    'Hydrogen',
+    'hydrogen',
+]
+
+for test in tests:
+    assert hydrogen == table.element(test)
